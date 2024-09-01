a6c367323e91b92fd48d5aedb87c1c2938042ed9
saeedbarcha <saeedbarcha77@gmail.com>
2024-09-01 05:06:38 +0500

    fix isssue somehow

--- src/app.js
+++ src/app.js
@@ -12,16 +12,21 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Use routes
 app.use('/api/v1/personal_loan', loanRoutes);
 
+
+// Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Sync Sequelize models
 sequelize.sync().then(() => {
   console.log('Database synced');
 }).catch((error) => {
   console.error('Error syncing database:', error);
 });
 
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
@@ -31,29 +36,3 @@ app.listen(port, () => {
 });
 
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const loanRoutes = require('../routes/loanRoutes');
-// const cors = require('cors');
-// const { sequelize } = require('./config');
-
-// const app = express();
-// const port = process.env.PORT || 8000;
-
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Use routes
-// app.use('/api/v1/personal_loan', loanRoutes);
-
-// // Sync Sequelize models
-// sequelize.sync().then(() => {
-//   console.log('Database synced');
-// }).catch((error) => {
-//   console.error('Error syncing database:');
-// });
-
-// app.listen(port,  () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
--- src/config.js
+++ src/config.js
@@ -1,9 +1,28 @@
 const { Sequelize } = require('sequelize');
-const config = require('../config/config.json')['development'];
+require('dotenv').config();
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432, // Default to 5432 if not set
+    dialect: 'postgres',
+    logging: false, // Disable logging for development
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Accept self-signed certificates
+      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000, // Maximum time (in milliseconds) that pool will try to get connection before throwing error
+      idle: 10000, // Maximum time (in milliseconds) that a connection can be idle before being released
+    },
+  }
+);
 
 module.exports = { sequelize };
