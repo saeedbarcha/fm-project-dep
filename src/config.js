d1b26356d957fbe89382adb0f7ec4e5310e4f617
saeedbarcha <saeedbarcha77@gmail.com>
2024-09-01 05:36:09 +0500

    fix isssue

--- src/config.js
+++ src/config.js
@@ -7,20 +7,20 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432, // Default to 5432 if not set
-    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres',
     logging: false, // Disable logging for development
     dialectOptions: {
       ssl: {
-        require: true,
+        require: process.env.DB_SSL === 'true', // Enable SSL based on environment variable
         rejectUnauthorized: false, // Accept self-signed certificates
       },
     },
     pool: {
       max: 5,
       min: 0,
-      acquire: 30000, // Maximum time (in milliseconds) that pool will try to get connection before throwing error
-      idle: 10000, // Maximum time (in milliseconds) that a connection can be idle before being released
+      acquire: 30000,
+      idle: 10000,
     },
   }
 );
